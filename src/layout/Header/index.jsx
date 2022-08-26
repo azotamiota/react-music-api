55788e59f0548e31c280ba66e1c3f74176faade4
azotamiota <azotamiota@gmail.com>
2022-08-26 16:18:04 +0100

    update navbar

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -7,12 +7,28 @@ const Header = () => {
         textDecoration: "underline",
     };
 
-
     return <>
         <header>
-            <NavLink className='nav-link' to="/" style={({ isActive }) => (isActive ? activeStyle : undefined)}>Home</NavLink>
-            <NavLink className='nav-link' to='songs' style={({ isActive }) => (isActive ? activeStyle : undefined)}>Search</NavLink>
-            <NavLink className='nav-link' to='about' style={({ isActive }) => (isActive ? activeStyle : undefined)}>About</NavLink>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                    <li className="nav-item active">
+                        <NavLink className="nav-link" to='/' style={({ isActive }) => (isActive ? activeStyle : undefined)}>Home</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to='songs' style={({ isActive }) => (isActive ? activeStyle : undefined)}>Search</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to='about' style={({ isActive }) => (isActive ? activeStyle : undefined)}>About</NavLink>
+                    </li>
+
+                    </ul>
+                </div>
+            </nav>
+
         </header>
             <Outlet/> 
         
